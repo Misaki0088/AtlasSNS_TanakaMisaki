52929567e20f30ca33b879492f911d8bef62d04d
DESKTOP-A92J59S\misak <misaki.tanaka0827@gmail.com>
2025-02-01 21:38:41 +0900

    検索できるようになった（検索画面一覧にまだ自分がいる）

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,7 @@ class UsersController extends Controller
 
     public function search(Request $request)
     {
+
         $keyword = $request->input('keyword');
         if(!empty($keyword)){
 // 変数（箱）＝箱の中身（検索する方法）User←Model
@@ -17,7 +18,7 @@ class UsersController extends Controller
         $users = User::where('username','like', '%'.$keyword.'%')->get();
         }else{
         $users = User::all();
-        return view('users.search',['users'=>$users]);
         }
+        return view('users.search',['users'=>$users]);
     }
 }
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -12,24 +12,26 @@
 
     {{ Form::open(['url' => '/search','method' => 'GET']) }}
         @csrf
-
-    <input type="text" name="search" value="{{ request('search') }}" class="search_form" placeholder="ユーザー名" required>
+<!-- submitはinputの船ようなイメージで大体一緒にいることが多い（今回はsubmit号でinputのkeywordのものを乗せてく） -->
+    <input type="text" name="keyword" value="{{ request('keyword') }}" class="search_form" placeholder="ユーザー名" required>
     <button type="submit" class="Submit_button"><a><img src="images/search.png"></a></button>
     {{ Form::close() }}
 
 <!-- $usersはUserControllerでUserテーブルのすべてのことだよって書いた-->
 <!-- $users->isEmpty（←だからUserカラムに無ければ検索結果はありませんって表示してくれってこと）-->
-    @if ($users->isEmpty())
+@if ($users->isEmpty())
     <p>検索結果はありません。</p>
 @else
     <ul>
 <!-- んで$usersはUserカラムの全てって事なんだけど$userばっかでわかんなくなるから一旦$userって表記にするね -->
-<!-- $user->email（←んで$userの中のusernameカラムを一覧で出してくれってこと -->
+<!-- $user->username（←んで$userの中のusernameカラムを一覧で出してくれってこと -->
         @foreach ($users as $user)
+        <div class="search_user_icon"><a><img src="images/icon1.png"></a></div>
             <li>{{ $user->username }}</li>
         @endforeach
     </ul>
 @endif
+
 
 
 </x-login-layout>
