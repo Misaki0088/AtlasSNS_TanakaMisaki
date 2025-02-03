fe4cc341cd05f071b32afff153fd89b8d98abb80
DESKTOP-A92J59S\misak <misaki.tanaka0827@gmail.com>
2025-02-03 20:17:16 +0900

    検索画面の一覧から自分が表示されないようになった

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,13 +11,17 @@ class UsersController extends Controller
     {
 
         $keyword = $request->input('keyword');
+        $currentUserId = auth()->id();
         if(!empty($keyword)){
 // 変数（箱）＝箱の中身（検索する方法）User←Model
 // 「usersテーブルのusernameカラムからあいまい検索し、検索にヒットしたレコードを取得」
 // $users=User::all(←ドルusersって名前の表記でUserテーブルの全てのことだよって記述)
-        $users = User::where('username','like', '%'.$keyword.'%')->get();
+        $users = User::where('username','like', '%'.$keyword.'%')
+                    ->where('id', '!=', $currentUserId) 
+                    ->get(); // 自分を除外->get();
         }else{
-        $users = User::all();
+        $users = User::where('id', '!=', $currentUserId)  // 自分を除外
+        ->get();
         }
         return view('users.search',['users'=>$users]);
     }
