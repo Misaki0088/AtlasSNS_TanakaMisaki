54a1579a6cc94c28788935b01e050528f8b60d88
DESKTOP-A92J59S\misak <misaki.tanaka0827@gmail.com>
2025-02-06 21:13:20 +0900

    フォロー、フォロワー、ユーザー検索のボタン設置

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,8 +17,8 @@ class UsersController extends Controller
 // 「usersテーブルのusernameカラムからあいまい検索し、検索にヒットしたレコードを取得」
 // $users=User::all(←ドルusersって名前の表記でUserテーブルの全てのことだよって記述)
         $users = User::where('username','like', '%'.$keyword.'%')
-                    ->where('id', '!=', $currentUserId) 
-                    ->get(); // 自分を除外->get();
+                    ->where('id', '!=', $currentUserId)// 自分を除外
+                    ->get();
         }else{
         $users = User::where('id', '!=', $currentUserId)  // 自分を除外
         ->get();
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -35,19 +35,23 @@
     </div>
     <div id="side-bar">
       <div id="confirm">
-        <p>〇〇さんの</p>
+      <div id="clear">
+      @if (session('username'))
+  <p>{{ session('username') }}さんの</p>
+  @endif
         <div>
           <p>フォロー数</p>
           <p>〇〇名</p>
         </div>
-        <p class="btn"><a href="follow-list">フォローリスト</a></p>
+        <p class="btn btn-primary"><a href="follow-list">フォローリスト</a></p>
         <div>
           <p>フォロワー数</p>
           <p>〇〇名</p>
         </div>
-        <p class="btn"><a href="follower-list">フォロワーリスト</a></p>
+        <p class="btn btn-primary"><a href="follower-list">フォロワーリスト</a></p>
       </div>
-      <p class="btn"><a href="search">ユーザー検索</a></p>
+      <p class="btn btn-primary"><a href="search">ユーザー検索</a></p>
+      </div>
     </div>
   </div>
   <footer>
