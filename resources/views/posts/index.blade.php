7d4efdac3eea4f2d3c46b88e2305655a5430f0e3
DESKTOP-A92J59S\misak <misaki.tanaka0827@gmail.com>
2025-04-01 22:11:25 +0900

    HTML見直し、修正したが変わらず…

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,52 +14,53 @@
     <form action="/tweet" method="post">
         @csrf
             <!-- 投稿フォーム -->
+        <div class="form">
             <textarea name="tweet" value="" class="Input_form" placeholder="投稿内容を入力してください" required></textarea>
 
             <!-- 投稿ボタン -->
             <input type="image" class="Submit_button" src="images/post.png" alt="投稿ボタン" id="submitbutton">
         </div>
     </form>
+</div>
 
 <!-- 投稿の表示 -->
-    @foreach ($posts as $post)
-    <div class="tweet">
+@foreach ($posts as $post)
+<div class="tweet">
         <!-- アイコンの表示 -->
-    <div class="post"><a><img src="{{ asset('storage/' . $post->user->icon_image) }}" alt="アイコン" id="icon"></a></div>
+    <div class="post"><a><img src="{{ asset('storage/' . $post->user->icon_image) }}" alt="アイコン" id="icon"></a>
+    </div>
         <!-- ユーザー名の表示 -->
         <p class="post_username">{{ $post->user->username }}</p>
         <!-- 日時表示 -->
         <p class="post-time">{{ $post->created_at->format('Y-m-d H:i') }}</p>
-    </div>
+</div>
     <!-- 投稿の表示 -->
     {{ $post->post }}
 
-    <div class="button-container">
+<div class="button-container">
         <!-- 編集ボタンの設置 -->
         <a class="update_button js-modal-open" data-id="{{ $post->id }}" data-post="{{ $post->post }}">
-        <img src="images/edit.png" alt="編集ボタン" id="update_button"></a></button>
-
+        <img src="images/edit.png" alt="編集ボタン" id="update_button"></a>
         <!-- 削除ボタンの設置 -->
         <td><a id="delete-button" href="/post/{{$post->id}}/delete" onclick="return confirm('この投稿を削除します。よろしいでしょうか？')">
         <img src="images/trash.png" onmouseover="this.src='images/trash-h.png'" onmouseout="this.src='images/trash.png'" alt="削除ボタン" id="delete_button">
         <!-- <img src="images/trash-h.png" alt="ホバーボタン" id="delete_h_button"> -->
         </a></td>
-
-    </div>
-        @endforeach
+</div>
+@endforeach
 
 <!-- モーダルの中身 -->
-    <div class="modal js-modal">
-        <div class="modal__bg js-modal-close"></div>
-            <div class="modal__content">
-                <form action="/post/update" method="POST">
-                    <textarea name="post_content" class="modal_post"></textarea>
-                    <input type="hidden" name="post_id" class="modal_id" value="">
-                    <input type="submit" value="更新">
-                    {{ csrf_field() }}
-                </form>
-            </div>
-    </div>
+<div class="modal js-modal">
+    <div class="modal__bg js-modal-close"></div>
+        <div class="modal__content">
+            <form action="/post/update" method="POST">
+                <textarea name="post_content" class="modal_post"></textarea>
+                <input type="hidden" name="post_id" class="modal_id" value="">
+                <input type="submit" value="更新">
+                {{ csrf_field() }}
+            </form>
+        </div>
+</div>
 
 
 </x-login-layout>
